2cf75cba31b36bb83b97150cd1595ced43d06058
mdahmen <manuel.dahmen@gmail.com>
2015-05-04 11:25:28 +0200

    création d'un nouveau composant
    Liens entre "data"

--- app/composant/editlink/contents.php
+++ app/composant/editlink/contents.php
@@ -1,3 +1,39 @@
 <?php
+require_once("../../config.php");
+?>
+<script language="text/javascript">
+    function addRelations(id)
+    {
+        
+    }
+</script>
+<?php
+/**
+ * composant/editlink/contents.php
+ * Edition des liens entre éléments. Ou "browser 2"
+ */
+$id = rawurldecode(filter_input(INPUT_GET, "dbdoc"));
 
-
+/**
+ * Loads relations as list of linked items
+ * <span></span><a href="manageRelation(rel_id)">Relation</a><span></span>
+ * @param type $id particular id to manage.
+ */
+function loadSelectDataParent($id)
+{
+    
+}
+/***
+ * Load all data for inclusion in element {{id}}
+ */
+function loadSelectDataAllData($id)
+{
+    
+}
+?>
+<select size="10" name="<?= $id ?>">
+    <?php
+        loadSelectDataAllData();
+    ?>
+</select>
+<input type="button" name="addThis" value="Ajouter les relations" onclick="addRelations(<?= $id; ?>)"/>
